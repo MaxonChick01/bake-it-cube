0f17de7eec29866c6e960fab054a1bfca7ba7bba
MaxonChick01 <20maximilianus01@gmail.com>
2024-04-03 15:07:05 +0300

    upd

--- SHOP/urls.py
+++ SHOP/urls.py
@@ -14,10 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve as mediaserve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls'))
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
+                mediaserve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(f'^{settings.STATIC_URL.lstrip("/")}(?P<path>.*)$',
+                mediaserve, {'document_root': settings.STATIC_ROOT}),
+    ]
+
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Category, Product
 
 
 def main(request):
-    return render(request, 'index.html')
+    context = {}
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    context['label'] = "YourMeal"
+    context['categories'] = categories
+    context['products'] = products
+    return render(request, 'index.html', context)
