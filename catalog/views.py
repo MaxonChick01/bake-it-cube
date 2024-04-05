b363f259a6684d114fd9820cec9a2ee183cda0f6
MaxonChick01 <20maximilianus01@gmail.com>
2024-04-05 16:08:02 +0300

    upd

--- catalog/views.py
+++ catalog/views.py
@@ -3,9 +3,14 @@ from .models import Category, Product
 
 
 def main(request):
+    category = request.GET.get('category', '0')
+    if category == '0':
+        products = Product.objects.all()
+    else:
+        products = Product.objects.filter(category=category)
+
     context = {}
     categories = Category.objects.all()
-    products = Product.objects.all()
     context['label'] = "YourMeal"
     context['categories'] = categories
     context['products'] = products
